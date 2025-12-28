d7dead7b1a7f1bcf8e28dfb214d3f09f4f48df22
T2710 <thomas.97tk@gmail.com>
2025-12-28 15:45:34 +0100

    adjusting optimizer according to GPT-3 Paper (not infos for GPT2)

--- my_gpt2/source/trainier.py
+++ my_gpt2/source/trainier.py
@@ -1,5 +1,6 @@
 import math
 from dataclasses import dataclass
+import inspect
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
@@ -9,6 +10,7 @@ To optimize training time:
 1. Max out Batch Size to still fit in GPU memory
 2. Choose Float network precision (e.g., float32 , 16..) if supported by hardware
 3. Use torch.compile
+4. Numbers devisible by 2, 8, etc. are faster due to gpu architecture
 """
 
 # 1. SSFT with The Pile Dataset 2. SFT with OpenAssistent Dataset
@@ -38,10 +40,16 @@ class CausalSelfAttention(nn.Module):
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  
         # compute attention scores
+        """
+        # original implementation from attention is all you need paper
         att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
         att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
         att = F.softmax(att, dim=-1)
-        y = att @ v  
+        y = att @ v 
+        """
+        # more efficient implementation from Flash Attention paper
+        y = F.scaled_dot_product_attention(q, k, v, is_causal=True)
+
         y = y.transpose(1, 2).contiguous().view(B, T, C)  # re-assemble all head outputs side by side
         y = self.c_proj(y) # output projection
         return y
@@ -142,7 +150,43 @@ class GPT(nn.Module):
             loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
         return logits, loss
 
+    """
+    This method configures the optimizer for the model.
+    It collects all trainable parameters and splits them into two groups:
+    1) Parameters with 2 or more dimensions (e.g. weight matrices, embeddings),
+       which receive weight decay for regularization.
+    2) Parameters with fewer than 2 dimensions (e.g. biases, LayerNorm weights),
+       which do NOT receive weight decay to avoid harming sensitive parameters. 
+    Weight decay is a regularization technique. It slightly pulls the weights toward zero at every optimization step.
+    These two groups are passed to AdamW with different weight decay settings.
+    The method also automatically enables the faster "fused" AdamW version
+    when running on CUDA, if it is available.
+    
+    """
+    def configure_optimizers(self, weight_decay, learning_rate, device_type):
+        # start with all of the candidate parameters (that require grad)
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+        # create optim groups. Any parameters that is 2D will be weight decayed, otherwise no.
+        # i.e. all weight tensors in matmuls + embeddings decay, all biases and layernorms don't.
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+        optim_groups = [
+            {'params': decay_params, 'weight_decay': weight_decay},
+            {'params': nodecay_params, 'weight_decay': 0.0}
+        ]
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in nodecay_params)
 
+        print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
+        print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+        # Create AdamW optimizer and use the fused version if it is available
+        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available and device_type == "cuda"
+        print(f"using fused AdamW: {use_fused}")
+        optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8, fused=use_fused)
+        return optimizer
+    
     """
     Initialize weights using huggingface's method for GPT-2. Allows us to load pretrained models as well.
     """
@@ -196,6 +240,8 @@ class GPT(nn.Module):
         return model
 
 
+
+
 # ------------------- Training Setup -------------------
 import tiktoken
 import time
@@ -235,27 +281,53 @@ torch.cuda.manual_seed(1337)
 train_loader = DataLoaderLite(B = 4, T = 1024)
 torch.set_float32_matmul_precision('high')
 
-model = GPT(GPTConfig())
+# Numbers devisible by 2 are faster due to gpu architecture, Add fake vocab size to make it devisible by 2, 8 etc.
+model = GPT(GPTConfig(vocab_size=50304))
 model.to(device)
 # model = torch.compile(model)
 
-# optimize the model
-optimizer = torch.optim.AdamW(model.parameters(),lr=3e-4)
-for i in range(50):
+
+# Learning Rate adjustment over time
+max_lr = 6e-4
+min_lr = max_lr *0.1
+warmup_steps = 10
+max_steps = 50
+def get_lr(it):
+    # 1. linear warmup for warmup_steps
+    if it < warmup_steps:
+        return max_lr * (it + 1) / warmup_steps
+    # 2. if it > max_steps: return min_lr
+    if it > max_steps:
+        return min_lr
+    # 3. cosine decay down to min_lr 
+    decay_ratio = (it - warmup_steps) / (max_steps - warmup_steps)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
+    return min_lr + coeff * (max_lr - min_lr)
+
+# optimize the model according to GPT-3 Paper, because they didnt publish in the GPT-2 paper
+optimizer = model.configure_optimizers(weight_decay=0.1, learning_rate= 6e-4, device_type=device)
+
+for step in range(max_steps):
     t0 = time.time()
     x, y = train_loader.next_batch()
     x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
     with torch.autocast(device_type=device, dtype=torch.bfloat16):
         logits, loss = model(x, y)
-        import code; code.interact(local=dict(locals()))
     loss.backward()
+    # Clip gradients to prevent exploding gradients for example in bad Batches
+    norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+    # determine the learning rate for this step
+    lr = get_lr(step)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
     optimizer.step()
     torch.cuda.synchronize()
     t1 = time.time()
     dt = (t1 - t0)*1000
     tokens_per_step = (train_loader.B * train_loader.T) / (t1 - t0)
-    print(f"Step {i}, loss: {loss.item()}, dt: {dt:.2f}ms , tokens/sec: {tokens_per_step:.2f}")
+    print(f"Step {step} | loss: {loss.item()} | lr: {lr:.4e} | norm: {norm:.4f} | dt: {dt:.2f}ms | tokens/sec: {tokens_per_step:.2f}")
 
 
 # num_return_sequences = 5
